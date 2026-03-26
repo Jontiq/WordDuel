db4cf314bda6c2dffb635fb87f078fc2c47e721f
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-03-26 13:32:17 +0100

    RoundModel

--- WordDuel.DAL/Models/RoundModel.cs
+++ WordDuel.DAL/Models/RoundModel.cs
@@ -6,5 +6,20 @@ namespace WordDuel.DAL.Models
 {
     public class RoundModel
     {
+        public int Id { get; set; }
+        public int MatchId { get; set; }
+        public int RoundNumber { get; set; }
+        public string StartingWord { get; set; } = string.Empty;
+        public string CurrentWord { get; set; } = string.Empty;
+        public string State { get; set; } = string.Empty;
+        public int? WinnerPlayerId { get; set; }
+        public int StartingPlayerId { get; set; }
+
+        public MatchModel Match { get; set; } = null!;
+        public PlayerModel? WinnerPlayer { get; set; }
+        public PlayerModel? StartingPlayer { get; set; }
+
+        public ICollection<MoveModel> Moves { get; set; } = new List<MoveModel>();
+
     }
 }
