7a6ee47c78c91470640c0359f2e3aff0572139b5
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-03-26 13:26:26 +0100

    MatchModel, ev ta bort navprops

--- WordDuel.DAL/Models/MatchModel.cs
+++ WordDuel.DAL/Models/MatchModel.cs
@@ -6,5 +6,21 @@ namespace WordDuel.DAL.Models
 {
     public class MatchModel
     {
+        public int Id { get; set; }
+
+        public string State { get; set; } = string.Empty;
+        public int BestOf { get; set; }
+
+        public int CurrentRoundId { get; set; }
+        public int CurrentPlayerId { get; set; }
+        public int? WinnerPlayerId { get; set; }
+        public int TurnTimeSeconds { get; set; }
+
+        public RoundModel CurrentRound { get; set; }
+        public PlayerModel CurrentPlayer { get; set; }
+        public PlayerModel WinnerPlayer { get; set; }
+
+        public ICollection<PlayerModel> Players { get; set; } = new List<PlayerModel>();
+        public ICollection<RoundModel> Rounds { get; set; } = new List<RoundModel>();
     }
 }
