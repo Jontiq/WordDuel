f5057c0ccd81748f1bff1e42c6ce13ece1a1f94b
pullsina <sovya22@gmail.com>
2026-03-26 13:12:47 +0100

    added CreateGameTests

--- WordDuel.BLL/GameLogicServices/GameService.cs
+++ WordDuel.BLL/GameLogicServices/GameService.cs
@@ -7,13 +7,26 @@ namespace WordDuel.BLL.GameLogicServices
 {
     public class GameService
     {
+        // <summary>
+        // Creates a new game instance with the required number of wins.
+        // We use "RoundsToWin" instead of total rounds because the game follows a "best of" format.
+        // This means the match ends as soon as a player reaches the required number of wins,
+        // so the total number of rounds is dynamic and does not need to be stored.
+        // ex. maxRounds = (RoundsToWin * 2) - 1;
+        // </summary>
         public Game CreateGame(int roundsToWin)
         {
-            return new Game
+            Game game = new Game
             {
                 RoundsToWin = roundsToWin,
                 State = Enums.GameState.WaitingForPlayers
             };
+            if (roundsToWin <= 0)
+            {
+                throw new ArgumentException("RoundsToWin must be greater than 0");
+            }
+
+            return game;
         }
 
     }
---
+++ WordDuel.UnitTests/GameLogicServiceTests.cs
@@ -0,0 +1,34 @@
+﻿
+using WordDuel.BLL.Enums;
+using WordDuel.BLL.GameLogicServices;
+
+namespace WordDuel.UnitTests
+{
+    public class GameLogicServiceTests
+    {
+        [Fact]
+        public void CreateGame_ShouldReturnGameWithCorrectRoundsToWinAndWaitingForPlayersState()
+        {
+            // Arrange
+            var service = new GameService();
+
+            // Act
+            var result = service.CreateGame(3);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(3, result.RoundsToWin);
+            Assert.Equal(GameState.WaitingForPlayers, result.State);
+        }
+        
+        [Fact]
+        public void CreateGame_ShouldThrowException_WhenRoundsToWinIsLessThanOrEqualToZero()
+        {
+            // Arrange
+            var service = new GameService();
+
+            // Act and Assert
+            Assert.Throws<ArgumentException>(() => service.CreateGame(0));
+        }
+    }
+}
