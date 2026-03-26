c3e64f498fef019fb08a860835ec95353c122240
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-03-26 13:33:51 +0100

    MoveModel

--- WordDuel.DAL/Models/MoveModel.cs
+++ WordDuel.DAL/Models/MoveModel.cs
@@ -6,5 +6,13 @@ namespace WordDuel.DAL.Models
 {
     public class MoveModel
     {
+        public int Id { get; set; }
+        public int RoundId { get; set; }
+        public int PlayerId { get; set; }
+        public string Word { get; set; } = string.Empty;
+        public int MoveNumber { get; set; }
+
+        public RoundModel Round { get; set; } = null!;
+        public PlayerModel Player { get; set; } = null!;
     }
 }
