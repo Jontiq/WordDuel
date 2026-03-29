209fc3cfbc881671a1735f021f75a228a5d4dedb
Jontiq <99jonathancar@gmail.com>
2026-03-29 19:54:36 +0200

    Created tests for main page

---
+++ WordDuel.UI.Tests/Tests/Features/Mainpage.feature
@@ -0,0 +1,31 @@
+﻿Feature: Mainpage
+
+Will be the landing page for the user where they will be able to set up the game with rules and host a game, or join an already configured game.
+
+Scenario: Page is loaded with standard game-settings
+	Given I am on the mainpage
+	Then "3 set" is selected
+	And "30 sek" is selected
+
+Scenario: Change sets
+	Given I am on the mainpage
+	When I click on "5 set"
+	Then "5 set" is selected
+	And "3 set" is not selected
+
+
+Scenario: Change roundtime
+	Given I am on the mainpage
+	When I click on "15 sek"
+	Then "15 sek" is selected
+	And "30 sek" is not selected
+
+Scenario: Click "Host game"
+    Given I am on the mainpage
+    When I click on "HOST GAME"
+    Then The game-state is "waiting"
+
+Scenario: Click "Join game"
+    Given I am on the mainpage
+    When I click on "JOIN GAME"
+    Then The game-state is "waiting"
---
+++ WordDuel.UI.Tests/Tests/Steps/MainpageSteps.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Playwright;
+using Reqnroll;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordDuel.UI.Tests.Tests.Steps
+{
+    [Binding]
+    public class LobbySteps
+    {
+        private IPage _page = null!;
+        private IBrowser _browser = null!;
+        private IPlaywright _playwright = null!;
+
+        [BeforeScenario]
+        public async Task Setup()
+        {
+            _playwright = await Playwright.CreateAsync();
+            _browser = await _playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            _page = await _browser.NewPageAsync();
+        }
+
+        [AfterScenario]
+        public async Task Teardown()
+        {
+            await _browser.CloseAsync();
+            _playwright.Dispose();
+        }
+
+        [Given("I am on the mainpage")]
+        public async Task GivenIAmOnTheMainpage()
+        {
+            await _page.GotoAsync("http://localhost:5227");
+        }
+
+        [Then("{string} is selected")]
+        public async Task ThenIsSelected(string label)
+        {
+            var chip = _page.Locator(".chip.selected", new() { HasTextString = label });
+            await Assertions.Expect(chip).ToBeVisibleAsync();
+        }
+
+        [When("I click on {string}")]
+        public async Task WhenIClickOn(string label)
+        {
+            var locator = _page.Locator(".chip, button", new() { HasTextString = label });
+            await locator.ClickAsync();
+        }
+
+        [Then("{string} is not selected")]
+        public async Task ThenIsNotSelected(string label)
+        {
+            var chip = _page.Locator(".chip.selected", new() { HasTextString = label });
+            await Assertions.Expect(chip).Not.ToBeVisibleAsync();
+        }
+
+        [Then("The game-state is {string}")]
+        public async Task ThenTheGameStateIs(string expectedState)
+        {
+            await Assertions.Expect(_page.Locator("#di-gamestate"))
+                    .ToHaveTextAsync(expectedState);
+        }
+    }
+}
