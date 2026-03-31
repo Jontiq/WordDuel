74c1bb3c29e5191dd24a04c1d543114f06d29c7b
EmilAlexLarsson <emilalexlarsson@gmail.com>
2026-03-31 14:20:56 +0200

    Matchrepo interface och start på matchrepo

---
+++ WordDuel.DAL/Interfaces/IMatchRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WordDuel.DAL.Models;
+
+namespace WordDuel.DAL.Interfaces
+{
+    public interface IMatchRepository
+    {
+        Task<MatchModel?> GetByIdAsync(int id); // hämtar en match baserat på id
+        Task<MatchModel> CreateAsync(MatchModel match); // skapar en ny match
+        Task<MatchModel> UpdateAsync(MatchModel match); // uppdaterar en match
+        Task<bool> DeleteAsync(int id); // tar bort en match baserat på id
+        Task<IEnumerable<MatchModel>> GetAllAsync(); // hämtar alla matcher
+        Task<IEnumerable<MatchModel>> GetActiveMatchesAsync(); // hämtar alla aktiva matcher (t.ex. matcher som inte är avslutade)
+    }
+}
---
+++ WordDuel.DAL/Repositories/MatchRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WordDuel.DAL.Interfaces;
+using WordDuel.DAL.Models;
+
+namespace WordDuel.DAL.Repositories
+{
+    public class MatchRepository : IMatchRepository
+    {
+        public Task<MatchModel> CreateAsync(MatchModel match)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MatchModel>> GetActiveMatchesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MatchModel>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MatchModel?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MatchModel> UpdateAsync(MatchModel match)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
